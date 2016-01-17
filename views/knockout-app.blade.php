0cf6f6be4fa74bbd0aebc90b1118851bc37a966b
Marty Eason <dev@zawntech.com>
2016-01-17 17:15:23 -0500

    Update

--- views/knockout-app.blade.php
+++ views/knockout-app.blade.php
@@ -1,6 +1,34 @@
 <script>
 function KnockoutApp() {
 
+    // Reference KnockoutApp
+    var self = this;
+
+    // Define viewPort properties.
+    self.viewPort =
+    {
+        // Window width
+        width: ko.observable(0),
+
+        // Window height
+        height: ko.observable(0),
+
+        // Update the view port dimensions.
+        update: function() {
+            self.viewPort.width( $(window).width() );
+            self.viewPort.height( $(window).width() );
+        }
+    };
+
+    // Define the initializer function.
+    var initialize = function() {
+        $(document).ready(function(){
+            self.viewPort.update();
+        });
+    };
+
+    // Initialize the Knockout application.
+    initialize();
 }
 // Create the KnockoutApp instance.
 var App = new KnockoutApp();
